3a34eee1670044f3b81bae4aa2cdeaf6189eed82
Chaimaa-H <chham7702@gmail.com>
2023-07-14 10:44:12 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,18 +6,14 @@
  */
 int main(void)
 {
-int digit1, digit2;
-for (digit1 = 0; digit1 <= 9; digit1++)
+int i;
+for (i = 0; i < 10; i++)
 {
-for (digit2 = digit1 + 1; digit2 <= 9; digit2++)
-{
-putchar(digit1 + '0');
-putchar(digit2 + '0');
-if (digit1 != 8 || digit2 != 9)
+putchar(i + '0');
+if (i != 9)
 {
 putchar(',');
 putchar(' ');
-}
 }
 }
 putchar('\n');
